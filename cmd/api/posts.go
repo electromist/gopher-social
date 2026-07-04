2ce3f932032cc07d3989eb2d921146b66fed9c20
electromist <electromist.dev@gmail.com>
2026-07-04 10:59:35 +0530

    internal errors package

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -19,7 +19,8 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	var payload CreatePostPayload
 
 	if err := readJSON(w, r, &payload); err != nil {
-		writeJSONError(w, http.StatusBadRequest, err.Error())
+		// Yahan badRequestResponse use karein
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
@@ -27,53 +28,53 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 		Title:   payload.Title,
 		Content: payload.Content,
 		Tags:    payload.Tags,
-		// TODO: Change after auth
-		UserID: 1,
+		UserID:  1,
 	}
 
 	ctx := r.Context()
 
 	if err := app.store.Posts.Create(ctx, post); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		// Yahan internalServerError use karein
+		app.internalServerError(w, r, err)
 		return
 	}
 
 	if err := writeJSON(w, http.StatusCreated, post); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		// Yahan internalServerError use karein
+		app.internalServerError(w, r, err)
 		return
 	}
 }
 
 func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
-	// 1. URL se parameter nikalna (e.g. /posts/123 me se '123' nikalega)
 	idParam := chi.URLParam(r, "postID")
 
-	// 2. String ko int64 me convert karna
 	id, err := strconv.ParseInt(idParam, 10, 64)
 	if err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		// Yahan internalServerError (ya badRequest bhi kar sakte ho)
+		app.internalServerError(w, r, err)
 		return
 	}
 
 	ctx := r.Context()
 
-	// 3. Database method call karna
 	post, err := app.store.Posts.GetByID(ctx, id)
 	if err != nil {
 		switch {
 		case errors.Is(err, store.ErrNotFound):
-			// Post na milne par 404 Status Not Found bhejo
-			writeJSONError(w, http.StatusNotFound, store.ErrNotFound.Error())
+			// Yahan notFoundResponse use karein
+			app.notFoundResponse(w, r, err)
 		default:
-			// Kisi aur problem ke liye 500 error bhejo
-			writeJSONError(w, http.StatusInternalServerError, err.Error())
+			// Yahan internalServerError use karein
+			app.internalServerError(w, r, err)
 		}
 		return
 	}
 
-	// 4. Sab sahi hone par HTTP 200 OK aur JSON bhej do
 	if err := writeJSON(w, http.StatusOK, post); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		// Yahan internalServerError use karein
+		app.internalServerError(w, r, err)
 		return
 	}
 }
+
