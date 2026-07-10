c659b693d70ca21f38691a0448c14046e8511e69
electromist <electromist.dev@gmail.com>
2026-07-10 08:25:40 +0530

    added pagination and sorting

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -47,8 +47,8 @@ func (app *application) mount() http.Handler {
 			r.Route("/{postID}", func(r chi.Router) {
 				r.Use(app.postsContextMiddleware)
 				r.Get("/", app.getPostHandler)
-				r.Delete("/", app.deletePostHandler) 
-				r.Patch("/", app.updatePostHandler) 
+				r.Delete("/", app.deletePostHandler)
+				r.Patch("/", app.updatePostHandler)
 			})
 		})
 		r.Route("/users", func(r chi.Router) {
@@ -57,6 +57,10 @@ func (app *application) mount() http.Handler {
 				r.Get("/", app.getUserHandler)
 				r.Put("/follow", app.followUserHandler)
 				r.Put("/unfollow", app.unfollowUserHandler)
+			})
+
+			r.Group(func(r chi.Router) {
+				r.Get("/feed", app.getUserFeedHandler)
 			})
 		})
 
